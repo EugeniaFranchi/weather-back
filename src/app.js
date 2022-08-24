a01ae716a276341a267fd1ddcf93992cb1730f6d
EugeniaFranchi <mfranchi@fi.uba.ar>
2022-08-24 18:04:06 -0300

    docs: add /v1 to openapi

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 import Fastify from 'fastify'
 import { getBaseEndpoint } from '../controllers/weather_controller.js'
+import swagger from '@fastify/swagger'
 
 const baseRoute = '/v1'
+const openapiOptions = {
+  mode: 'static',
+  routePrefix: `${baseRoute}/openapi`,
+  specification: {
+    path: './docs/openapi.yaml'
+  },
+  exposeRoute: true
+}
 
 const build = (options = {}) => {
   const app = Fastify(options)
+  app.register(swagger, openapiOptions)
 
   app.get(baseRoute, getBaseEndpoint)
 
